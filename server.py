aef89d39bd9a9bd339105d0601361cd967c6f931
Joonas Nordlund <jonaz92@gmail.com>
2016-10-25 12:06:36 +0300

    Added some routes. EI TOIMI.

--- server.py
+++ server.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from APICalls.Twitter import Twitter
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def root_route():
+    return render_template('main.html')
+
+
+@app.route('/<string:artist_slug>')
+def artist_route(artist_slug):
+    #TODO: artist = db.fetch(artist_slug)
+    # twitter = Twitter.Timeline(artist.twitter)
+    return render_template('artist.html')
+
+
+@app.route('/artist/add', methods=['GET', 'POST'])
+def add_artist():
+    if request.method == 'GET':
+        return render_template('add.html')
+    else:
+        
