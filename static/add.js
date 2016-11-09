fb3b7c08974db77e6369dea30df7ea2d829f3631
Juha-Matti Rahkola <jutarahk@student.jyu.fi>
2016-11-09 12:11:44 +0200

    Jäsenen lisäys kenttien lisääminen toimii nyt napista. Vaatii vielä korjauksia.

--- static/add.js
+++ static/add.js
@@ -1,10 +1,16 @@
 window.onload = function() {
+  idIncrement = 0;
   $('#memberButton').click(function(){
-    var newInput = document.createElement("input");
-    newInput.setAttribute('type', 'text');
+    var field = $( "#memberField" ).clone();
+    var id = "memberField" + idIncrement;
+    field.id = id;
+    idIncrement++;
+    field.appendTo( ".memberDiv" );
+    //var newInput = document.createElement("input");
+    //newInput.setAttribute('type', 'text');
 
     // add the newly created element and its content into the DOM
-    var currentDiv = document.getElementById("form-group");
-    document.body.insertBefore(newInput, currentDiv);
+    //var currentDiv = document.getElementById("form-group");
+    //document.body.insertBefore(newInput, currentDiv);
   });
 }
