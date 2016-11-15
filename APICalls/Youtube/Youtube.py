5128c0e69334286400a55609593235db1aa761e1
eejujole <ekimokoma@hotmail.com>
2016-11-15 13:04:09 +0200

    Youtube api file

--- APICalls/Youtube/Youtube.py
+++ APICalls/Youtube/Youtube.py
@@ -1,14 +1,20 @@
 import os
 import requests
+from YTube import YTube
 
 PLAYLIST_URL = "https://www.googleapis.com/youtube/v3/playlistItems"
 
 class Playlist:
 
     def __init__(self, playlistID):
-        self.playlistID = timeline
+        self.playlistID = playlistID
         self.videos = []
         self.fetchPlaylist()
 
     def fetchPlaylist(self):
-        r = requests.get(PLAYLIST_URL, params = {"part" = "snippet", "playlistID" : playlistID, "key" = os.environ["YOUTUBUE_KEY"],})
+        r = requests.get(PLAYLIST_URL, params = {"part" : "snippet", "playlistId" : self.playlistID, "key" : os.environ["YOUTUBE_KEY"]})
+	playlist = r.json()
+	print playlist
+	items = playlist["items"]
+	for video in items:
+		self.videos.append(YTube(video))
