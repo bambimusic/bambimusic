d5fc21836a074c137c3b5bced5ab646021700227
Juha-Matti Rahkola <jutarahk@student.jyu.fi>
2016-11-21 15:04:26 +0200

    Korjauksia add sivustolle. Lisasin albumien lisays osion.

--- static/add.js
+++ static/add.js
@@ -3,18 +3,19 @@ function newElement(elementName, className){
   element.className = className;
   return element;
 }
-idIncrement = 1;
+memberIdIncrement = 1;
+albumIdIncrement = 1;
 
 window.onload = function() {
   $('#memberButton').click(function(){
-    var membFld = newElement("div", "memberField" +idIncrement);
+    var membFld = newElement("div", "memberField" +memberIdIncrement);
     var membDiv = newElement("div", "col-sm-9");
     var instrDiv = newElement("div", "col-sm-3");
     var membInput = newElement("input", "form-control memberField");
-    membInput.id = "inputMembers" + idIncrement;
+    membInput.id = "inputMembers" + memberIdIncrement;
     membInput.placeholder = "Member";
     var instrInput = newElement("input", "form-control instrumentField");
-    instrInput.id = "inputMembersInstrument" + idIncrement;
+    instrInput.id = "inputMembersInstrument" + memberIdIncrement;
     instrInput.placeholder = "Instrument";
 
     membFld.appendChild(membDiv);
@@ -25,12 +26,39 @@ window.onload = function() {
     var div = document.getElementById( "memberDiv");
     div.appendChild(membFld);
 
-    idIncrement++;
+    memberIdIncrement++;
   });
 
   $('#removeMemberButton').click(function(){
-    $( ".memberField" +(idIncrement-1) ).remove();
-    if (idIncrement>1) {idIncrement--;}
+    $( ".memberField" +(memberIdIncrement-1) ).remove();
+    if (memberIdIncrement>1) {memberIdIncrement--;}
+  });
+
+  $('#albumButton').click(function(){
+    var albumFld = newElement("div", "albumField" +albumIdIncrement);
+    var albumDiv = newElement("div", "col-sm-9");
+    var yearDiv = newElement("div", "col-sm-3");
+    var albumInput = newElement("input", "form-control albumField");
+    albumInput.id = "inputAlbum" + albumIdIncrement;
+    albumInput.placeholder = "Album name";
+    var yearInput = newElement("input", "form-control yearField");
+    yearInput.id = "inputYear" + albumIdIncrement;
+    yearInput.placeholder = "Publish year";
+
+    albumFld.appendChild(albumDiv);
+    albumFld.appendChild(yearDiv);
+    albumDiv.appendChild(albumInput);
+    yearDiv.appendChild(yearInput);
+
+    var div = document.getElementById( "albumDiv");
+    div.appendChild(albumFld);
+
+    albumIdIncrement++;
+  });
+
+  $('#removeAlbumButton').click(function(){
+    $( ".albumField" +(albumIdIncrement-1) ).remove();
+    if (albumIdIncrement>1) {albumIdIncrement--;}
   });
 
 }
